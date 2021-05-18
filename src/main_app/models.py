391442fd8592105041846259ecc06d3f3e3d585a
KapelanJR <kacperszmitk@wp.pl>
2021-05-18 21:59:28 +0200

    models update

--- src/main_app/models.py
+++ src/main_app/models.py
@@ -14,17 +14,10 @@ class Ingerdients(models.Model):
         db_table = 'ingredients'
 
 
-class Units(models.Model):
-    unit_name = models.CharField(max_length=255)
-    # Ratio to our system default unit
-    conventer = models.FloatField()
-    class Meta:
-        db_table = 'units'
-
 class Recipes(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     last_modification = models.DateTimeField(auto_now_add=True)
-    descripction = models.CharField(max_length=511,default=None)
+    descripction = models.CharField(max_length=1023,default=None)
     recipe_name = models.CharField(max_length=255)
     overall_upvotes = models.IntegerField(default=0)
     last_month_upvotes = models.IntegerField(default=0)
@@ -32,6 +25,7 @@ class Recipes(models.Model):
     brewing_temperatue = models.FloatField()
     brewing_time = models.FloatField()
     mixing_time = models.FloatField()
+    tea_portion = models.FloatField()
     class Meta:
         db_table = 'recipes'
 
@@ -39,20 +33,12 @@ class Recipes(models.Model):
 class IngredientsRecipes(models.Model):
     recipe = models.ForeignKey(Recipes,on_delete=models.CASCADE)
     # Unit in which recipe was created
-    unit = models.ForeignKey(Units,on_delete=models.SET_NULL, null=True)
-    ammount_in_default_unit = models.FloatField()
+    ammount = models.FloatField()
     class Meta:
         db_table = 'ingredients_recipes'
 
 class UserSettings(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    liquid_unit = models.ForeignKey(Units,on_delete=models.SET_NULL, null=True,related_name='liquid_unit')
-    weight_unit = models.ForeignKey(Units,on_delete=models.SET_NULL, null=True,related_name='weight_unit')
-    # If True all recipes will be automaticly converted to choosen units
-    default_convert_to_user_unit = models.BooleanField(default=False)
-    # If True recipes saved by user will be converted to user choosen units
-    default_convert_to_user_unit_on_save = models.BooleanField(default=False)
-
     class Meta:
         db_table = 'user_settings'
     
@@ -62,15 +48,6 @@ class Machines(models.Model):
 
     class Meta:
         db_table = 'machines'
-class MachinesStores(models.Model):
-    machinie = models.ForeignKey(Machines,on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingerdients,on_delete=models.SET_NULL, null=True)
-    # In system default units
-    capacity = models.FloatField()
-    current_ammount = models.FloatField()
-
-    class Meta:
-        db_table = 'machines_stores'
 
 class FavoriteRecipes(models.Model):
     recipe = models.ForeignKey(Recipes,on_delete=CASCADE)
@@ -78,3 +55,8 @@ class FavoriteRecipes(models.Model):
 
     class Meta:
         db_table = 'favorite_recipes'
+
+class History(models.Model):
+    user = models.ForeignKey(User,on_delete=CASCADE)
+    recipe = models.ForeignKey(Recipes,on_delete=CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
