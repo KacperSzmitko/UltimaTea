59f7e325f5d791647a7a8b42f23cbb4b5236a4e8
Maciej Stefaniak <48411094+madragonse@users.noreply.github.com>
2021-06-14 13:57:34 +0200

    fix recipe edit width

--- src/main_app/admin.py
+++ src/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Recipes, FavoriteRecipes, IngredientsRecipes, Ingerdients, UserSettings, Machines, History, MachineContainers
+from .models import Recipes, FavoriteRecipes, IngredientsRecipes, Ingerdients, UserSettings, Machines, History, MachineContainers, Teas
 
 admin.site.register(Recipes)
 admin.site.register(FavoriteRecipes)
@@ -10,3 +10,4 @@ admin.site.register(UserSettings)
 admin.site.register(Machines)
 admin.site.register(History)
 admin.site.register(MachineContainers)
+admin.site.register(Teas)
